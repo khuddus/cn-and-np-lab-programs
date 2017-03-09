75e2833f28633693b096230e347bac6368522022
Abdul Khuddus <khuddus1@gmail.com>
2017-03-09 17:48:08 +0530

    Processes are change to verify inter process communication

--- 1b_pipes_in_two_processes.c
+++ 1b_pipes_in_two_processes.c
@@ -9,16 +9,17 @@ pipe(pipefd);
 pid=fork();
 if(pid==0){
 printf("\nChild Process");
-close(pipefd[0]);
-write(pipefd[1],"Data from child",30);
-
-}
-else{
-printf("\nParent Process");
 close(pipefd[1]);
 n=read(pipefd[0],buff,sizeof(buff));
  printf("\n size of the data%d",n); 
 printf("\n data :%s",buff);
+
+}
+else{
+printf("\nParent Process");
+close(pipefd[0]);
+write(pipefd[1],"Data from parent",30);
+
 
 }
 
