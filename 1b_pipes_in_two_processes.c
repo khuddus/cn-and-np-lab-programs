dac1aed841771a61bb992b4b7b5e076387fb408d
Abdul Khuddus <khuddus1@gmail.com>
2017-03-09 17:46:21 +0530

    Two process communication using pipes accomplished

---
+++ 1b_pipes_in_two_processes.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+main()
+{
+
+int pipefd[2],n,pid; 
+char buff[100]; 
+pipe(pipefd);
+pid=fork();
+if(pid==0){
+printf("\nChild Process");
+close(pipefd[0]);
+write(pipefd[1],"Data from child",30);
+
+}
+else{
+printf("\nParent Process");
+close(pipefd[1]);
+n=read(pipefd[0],buff,sizeof(buff));
+ printf("\n size of the data%d",n); 
+printf("\n data :%s",buff);
+
+}
+
+
+}
+
