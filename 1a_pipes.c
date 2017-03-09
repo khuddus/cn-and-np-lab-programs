3d44012bc1e6ee9147f46097413ed165b09aa26e
Abdul Khuddus <khuddus1@gmail.com>
2017-03-09 17:34:56 +0530

    First Program completed

---
+++ 1a_pipes.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+main()
+{
+
+int pipefd[2],n; char buff[100]; pipe(pipefd);
+
+printf("\nreadfd=%d",pipefd[0]);
+printf("\nwritefd=%d",pipefd[1]);
+
+write(pipefd[1],"helloworld",12);
+
+n=read(pipefd[0],buff,sizeof(buff));
+ printf("\n size of the data%d",n); 
+printf("\n data from pipe:%s",buff);
+
+}
+
